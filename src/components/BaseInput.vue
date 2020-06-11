d4c66636696080364234ab46b3647b1fb6ce1e97
Dave Longbottom <davelongbottom@gmail.com>
2020-06-11 19:58:22 +0930

    added BaseInput component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="field">
+        <label v-if="label">{{label}}</label>
+        <!-- <input @input="updateValue" :value="value" :placeholder="placeholder" type="text" /> -->
+        <input @input="updateValue" :value="value" v-bind="$attrs" />
+    </div>
+</template>
+
+<script>
+    export default {
+        inheritAttrs: false,
+        props: {
+            label: {
+                type: String,
+                default: ''
+            },
+            value: [String, Number],
+            // placeholder: {
+            //     type: String,
+            //     default: ''
+            // },
+        },
+        methods: {
+            updateValue(event) {
+                this.$emit('input', event.target.value)
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

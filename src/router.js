4eb2f4dfe039d47a93bad6af31f844fe8c7af40b
Dave Longbottom <davelongbottom@gmail.com>
2020-06-11 18:51:49 +0930

    catch all 404

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import EventCreate from './views/EventCreate.vue'
 import EventList from './views/EventList.vue'
 import EventShow from './views/EventShow.vue'
+import NotFound from './views/NotFound.vue'
 
 Vue.use(Router)
 
@@ -24,6 +25,15 @@ export default new Router({
       name: 'event-show',
       component: EventShow,
       props: true
-    }
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: NotFound
+    },
+    {
+      path: '*',
+      redirect: { name: "404" }
+    },
   ]
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Oops</h1>
+        <p>Page not found!</p>
+        <router-link :to="{name: 'event-list'}">Back to Home Page</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
